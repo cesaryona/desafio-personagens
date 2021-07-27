8f6ab8c53bc465866f3f340fa8ffc25889554e93
Cesar Yona <cesaryona_@outlook.com>
2021-07-26 22:22:21 -0300

    adding logs

--- src/main/java/com/dextra/service/CharacterService.java
+++ src/main/java/com/dextra/service/CharacterService.java
@@ -5,6 +5,8 @@ import com.dextra.model.Character;
 import com.dextra.model.CharacterRequest;
 import com.dextra.repository.CharacterRepository;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,23 +15,28 @@ import java.util.Optional;
 @Service
 @AllArgsConstructor
 public class CharacterService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CharacterService.class);
 
     private CharacterRepository characterRepository;
     private HouseService houseService;
 
     public List<Character> findAll(){
+        LOGGER.info("Starting method 'findAll'");
         return characterRepository.findAll();
     }
 
     public Character findById(String id) {
+        LOGGER.info("Starting method 'findById' for: {}", id);
         return characterRepository.findById(id).orElseThrow(() -> new NotFoundException(String.format("Character not found. Id: %s", id)));
     }
 
     public List<Character> findByHouse(String house) {
+        LOGGER.info("Starting method 'findByHouse' for: {}", house);
         return characterRepository.findByHouse(house).orElseThrow(() -> new NotFoundException(String.format("Character not found. House: %s", house)));
     }
 
     public Character insert(CharacterRequest request) {
+        LOGGER.info("Starting method 'insert'");
         return characterRepository.save(Character.builder()
                 .name(request.getName())
                 .role(request.getRole())
@@ -40,6 +47,7 @@ public class CharacterService {
     }
 
     public void update(String id, CharacterRequest request) {
+        LOGGER.info("Starting method 'update' for: {}", id);
         Character current = findById(id);
         Optional.ofNullable(request.getName()).ifPresent(current::setName);
         Optional.ofNullable(request.getRole()).ifPresent(current::setRole);
@@ -51,6 +59,7 @@ public class CharacterService {
     }
 
     public void deleteById(String id){
+        LOGGER.info("Starting method 'deleteById' for: {}", id);
         characterRepository.deleteById(id);
     }
 }
--- src/main/java/com/dextra/service/HouseService.java
+++ src/main/java/com/dextra/service/HouseService.java
@@ -4,6 +4,8 @@ import com.dextra.exception.NotFoundException;
 import com.dextra.model.House;
 import com.dextra.repository.HouseRepository;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,16 +13,19 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class HouseService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HouseService.class);
 
     private static final String HOUSE_NOT_FOUND = "House not found. Id: %s";
 
     private HouseRepository houseRepository;
 
     public List<House> findAll() {
+        LOGGER.info("Starting method 'findAll'");
         return houseRepository.findAll();
     }
 
     public House findById(String id) {
+        LOGGER.info("Starting method 'findById' for: {}", id);
         return houseRepository.findById(id).orElseThrow(() -> new NotFoundException(String.format(HOUSE_NOT_FOUND, id)));
     }
 }
